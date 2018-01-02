c9c256218b8c4f6b753eeebd4aac96dcf80766d3
grimalkin <loki@amarantha.net>
2018-01-02 16:48:11 +0000

    Publish to AWS

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,31 @@ module StarlightMonitor
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Initialize AWS IoTDataPlane Client
+    client = Aws::IoTDataPlane::Client.new(
+        region: 'us-west-2',
+        access_key_id: ENV['AWS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET'],
+        endpoint: 'https://a2q2k2ttlfw6v7.iot.us-west-2.amazonaws.com'
+    )
+
+    topic = "Starlight"
+
+    payload = {
+        test: "Hello from Observatory #{ENV['RAILS_ENV']}"
+    }.to_json
+
+    # Publish payload to the topic
+    resp = client.publish({
+                              topic: topic,
+                              qos: 1,
+                              payload: payload
+                          })
+
+    puts client.waiter_names
+
+
+
   end
 end
